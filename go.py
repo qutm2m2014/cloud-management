e813e8b8b414a95da6236ce198c5685dfae11c79
AUTHORNAME <AUTHOREMAIL>
2014-05-13 12:50:11 +1000

    Added db and template support

--- go.py
+++ go.py
@@ -1,23 +1,44 @@
-from flask import Flask
+from flask import Flask, render_template
+
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.debug = True
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://localhost:5432/m2m'
+app.config["SQLALCHEMY_ECHO"] = True
+db = SQLAlchemy(app)
 
 
-@app.route(u"/")
+class Positions(db.Model):
+    __tablename__ = 'positions'
+    id = db.Column('id', db.Integer, primary_key=True)
+    name = db.Column('name', db.String)
+    lat = db.Column('lat', db.Integer)
+    lng = db.Column('long', db.Integer)
+
+
+@app.route("/")
 def hello_world():
-    return "Hello World"
+    positions = Positions.query.all()
+    return render_template('positions.html', positions=positions)
 
 
-@app.route(u"/devices")
+# List devices and the last time they were heard from.
+@app.route(u"/devices", methods=["GET"])
 def list_devices():
     pass
 
 
-@app.route(u"/heartbeat", methods=["POST"])
-def register_heartbeat():
+# Devices will register and will be returned basic configuration data.
+@app.route(u"/devices", methods=["POST"])
+def register_device():
     pass
 
+
+# Is the device still alive?
+@app.route(u"/devices/<uuid>/heartbeat", methods=["POST"])
+def register_heartbeat():
+    pass
 
 if __name__ == "__main__":
     app.run()
